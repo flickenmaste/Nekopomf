ea098862121fdcaa791997104b0a3b496efb67a4
Terry Nguyen <terreh@terrehbyte.com>
2015-04-27 20:38:50 -0700

    Shitty don't upload option

--- Nekopomf/Nekopomf/Upload.cs
+++ Nekopomf/Nekopomf/Upload.cs
@@ -160,7 +160,8 @@ namespace Nekopomf
             encoder.Frames.Add(BitmapFrame.Create(bitmapSource));
             //encoder.Save(memoryStream);
 
-            string path = "./temp.png";
+            Directory.CreateDirectory(System.IO.Path.GetTempPath() + "Nekopomf/");
+            string path = System.IO.Path.GetTempPath() + "Nekopomf/temp.png";
             using (var fileStream = new FileStream(path, FileMode.Create))
             {
                 encoder.Save(fileStream);   // Save temp img
@@ -170,87 +171,102 @@ namespace Nekopomf
             #region Upload
             // credit to https://github.com/mavanmanen/Scotty/blob/master/Scotty/Program.cs for this stuff
 
-            byte[] fileArray;
-            using (var fileStream2 = new FileStream(path, FileMode.Open, FileAccess.Read))
+            if (((MainWindow) System.Windows.Application.Current.MainWindow).DisableUpload.IsChecked != true)
             {
-                fileArray = new byte[fileStream2.Length];
-                fileStream2.Read(fileArray, 0, System.Convert.ToInt32(fileStream2.Length));
-                fileStream2.Close();
-                fileStream2.Dispose();
-            }
-            byte[] xArray = Encoding.ASCII.GetBytes("------BOUNDARYBOUNDARY----\r\ncontent-disposition: form-data; name=\"id\"\r\n\r\n\r\n------BOUNDARYBOUNDARY----\r\ncontent-disposition: form-data; name=\"files[]\"; filename=\"temp.png\"\r\nContent-type: null\r\n\r\n");
-            byte[] boundaryByteArray = Encoding.ASCII.GetBytes("\r\n------BOUNDARYBOUNDARY----");
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://pomf.se/upload.php");
-            request.Method = "POST";
-
-            //Client
-            request.Accept = "*/*";
-            request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.102 Safari/537.36";
-
-            //Entity
-            request.ContentLength = fileArray.Length + xArray.Length + boundaryByteArray.Length;
-            request.ContentType = "multipart/form-data; boundary=----BOUNDARYBOUNDARY----";
-
-            //Miscellaneous                
-            request.Referer = "http://pomf.se/";
-
-            //Transport
-            request.KeepAlive = true;
-
-            string responseContent = null;
-
-            try
-            {
-                Stream requestStream = request.GetRequestStream();
-                requestStream.Write(xArray, 0, xArray.Length);
-                requestStream.Write(fileArray, 0, fileArray.Length);
-                requestStream.Write(boundaryByteArray, 0, boundaryByteArray.Length);
-                requestStream.Close();
-
-                WebResponse response = request.GetResponse();
-                requestStream = response.GetResponseStream();
-
-                StreamReader responseReader = new StreamReader(requestStream);
-                responseContent = responseReader.ReadToEnd();
-
-                responseReader.Close();
-                requestStream.Close();
-                response.Close();
-
-                responseReader.Dispose();
-                requestStream.Dispose();
-                response.Dispose();
-                request = null;
-                xArray = null;
-                fileArray = null;
-                boundaryByteArray = null;
-            }
-            catch (Exception eeek)
-            {
-                Console.Write(eeek);
-            }
-
-            if (responseContent != null)
-            {
-                JObject json = JObject.Parse(responseContent);
-                if (Convert.ToBoolean(json["success"]))
+                byte[] fileArray;
+                using (var fileStream2 = new FileStream(path, FileMode.Open, FileAccess.Read))
                 {
-                    JArray files = (JArray)json["files"];
-                    fileUrl = (string)files[0]["url"];
-                    files = null;
+                    fileArray = new byte[fileStream2.Length];
+                    fileStream2.Read(fileArray, 0, System.Convert.ToInt32(fileStream2.Length));
+                    fileStream2.Close();
+                    fileStream2.Dispose();
                 }
-                json = null;
+                byte[] xArray = Encoding.ASCII.GetBytes("------BOUNDARYBOUNDARY----\r\ncontent-disposition: form-data; name=\"id\"\r\n\r\n\r\n------BOUNDARYBOUNDARY----\r\ncontent-disposition: form-data; name=\"files[]\"; filename=\"temp.png\"\r\nContent-type: null\r\n\r\n");
+                byte[] boundaryByteArray = Encoding.ASCII.GetBytes("\r\n------BOUNDARYBOUNDARY----");
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://pomf.se/upload.php");
+                request.Method = "POST";
+
+                //Client
+                request.Accept = "*/*";
+                request.UserAgent = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.102 Safari/537.36";
+
+                //Entity
+                request.ContentLength = fileArray.Length + xArray.Length + boundaryByteArray.Length;
+                request.ContentType = "multipart/form-data; boundary=----BOUNDARYBOUNDARY----";
+
+                //Miscellaneous                
+                request.Referer = "http://pomf.se/";
+
+                //Transport
+                request.KeepAlive = true;
+
+                string responseContent = null;
+
+                try
+                {
+                    Stream requestStream = request.GetRequestStream();
+                    requestStream.Write(xArray, 0, xArray.Length);
+                    requestStream.Write(fileArray, 0, fileArray.Length);
+                    requestStream.Write(boundaryByteArray, 0, boundaryByteArray.Length);
+                    requestStream.Close();
+
+                    WebResponse response = request.GetResponse();
+                    requestStream = response.GetResponseStream();
+
+                    StreamReader responseReader = new StreamReader(requestStream);
+                    responseContent = responseReader.ReadToEnd();
+
+                    responseReader.Close();
+                    requestStream.Close();
+                    response.Close();
+
+                    responseReader.Dispose();
+                    requestStream.Dispose();
+                    response.Dispose();
+                    request = null;
+                    xArray = null;
+                    fileArray = null;
+                    boundaryByteArray = null;
+                }
+                catch (Exception eeek)
+                {
+                    Console.Write(eeek);
+                }
+
+                if (responseContent != null)
+                {
+                    JObject json = JObject.Parse(responseContent);
+                    if (Convert.ToBoolean(json["success"]))
+                    {
+                        JArray files = (JArray)json["files"];
+                        fileUrl = (string)files[0]["url"];
+                        files = null;
+                    }
+                    json = null;
+                }
+
+                // copy upload to clipboard
+                System.Windows.Clipboard.SetText("http://a.pomf.se/" + fileUrl, System.Windows.TextDataFormat.Text);    // Copy to clipboard
+                ((MainWindow)System.Windows.Application.Current.MainWindow).URLBox.Text = "http://a.pomf.se/" + fileUrl + " has been copied to the clipboard.";
+
             }
             #endregion
 
-            ((MainWindow)System.Windows.Application.Current.MainWindow).URLBox.Text = "http://a.pomf.se/" + fileUrl + " has been copied to the clipboard.";
+
+
+            // play da sound
             if (((MainWindow)System.Windows.Application.Current.MainWindow).AudioCheck.IsChecked != true)
                 (new System.Media.SoundPlayer("./Resources/eye.wav")).Play();   // Play audio
-            System.Windows.Clipboard.SetText("http://a.pomf.se/" + fileUrl, System.Windows.TextDataFormat.Text);    // Copy to clipboard
-
+            
+            // Log check
             if (((MainWindow)System.Windows.Application.Current.MainWindow).LogCheck.IsChecked == true)
             {
+                if (((MainWindow) System.Windows.Application.Current.MainWindow).DisableUpload.IsChecked == true)
+                {
+                    fileUrl = "          ";
+                }
+
                 using (var fileStream = new FileStream("./log.txt", FileMode.Append))   // Save log of pastes
                 {
                     string linky = "http://a.pomf.se/" + fileUrl + " " + System.DateTime.Now.ToString() + Environment.NewLine;
@@ -260,6 +276,7 @@ namespace Nekopomf
                 }
             }
 
+            // Store local copy
             if (((MainWindow)System.Windows.Application.Current.MainWindow).LocalCopyCheck.IsChecked == true)
             {
                 if (!Directory.Exists("./saved/"))
